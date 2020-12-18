154feee5a730041e0bf3a7950e0b915d0d5edee6
Zhangqiwu-hen <1102925457@qq.com>
2020-12-18 14:25:26 +0800

    2020.12.18-study

--- src/main/java/j2se/charactor/Hero.java
+++ src/main/java/j2se/charactor/Hero.java
@@ -6,14 +6,33 @@ public class Hero {
     public int damage;
     public int id;
 
-    static String copyright;
-
-    static {
-        System.out.println("初始化 copyright");
-        copyright = "版权由Riot Games公司所有";
+    public String getName() {
+        return name;
     }
 
-    public String toString(){
-        return name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Hero() {
+
+    }
+
+    public Hero(String string) {
+        name = string;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero [name=" + name + "]";
+    }
+
+    public boolean isDead() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void attackHero(Hero h2) {
+        System.out.println(this.name + " 正在攻击 " + h2.getName());
     }
 }
--- src/main/java/j2se/reflection/TestReflection.java
+++ src/main/java/j2se/reflection/TestReflection.java
@@ -2,26 +2,42 @@ package j2se.reflection;
 
 import j2se.charactor.Hero;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class TestReflection {
     public static void main(String[] args) {
-        System.out.println(getHero());
-    }
-
-    public static Hero getHero(){
         File f = new File("D:\\cs\\ideaProject\\project\\study\\src\\main\\java\\j2se\\reflection\\hero.config");
-        try(FileReader fr = new FileReader(f) ){
-            char[] all = new char[(int)f.length()];
-            fr.read(all);
-            String className = String.valueOf(all);
-            Class pClass = Class.forName(className);
-            Constructor c = pClass.getConstructor();
-            return (Hero) c.newInstance();
+        try(FileReader fr = new FileReader(f); BufferedReader br = new BufferedReader(fr)){
+            String[] str=new String[4];
+            for(int i=0;i< str.length;i++){
+                str[i]= br.readLine();
+            }
+
+            Class c1 = Class.forName(str[0]);
+            Constructor cc1 = c1.getConstructor();
+            Object h1 = cc1.newInstance();
+            Field f1 = c1.getField("name");
+            f1.set(h1,str[1]);
+            System.out.println(h1);
+
+            Class c2 = Class.forName(str[2]);
+            Constructor cc2 = c2.getConstructor();
+            Object h2 = cc2.newInstance();
+            Field f2 = c2.getField("name");
+            f2.set(h2,str[3]);
+            System.out.println(h2);
+
+            Method m1 = c1.getMethod("attackHero",Hero.class);
+            m1.invoke(h1,h2);
+
         } catch (Exception e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-            return null;
         }
     }
 }
